3d7294c2baec3113b8de170b290c34c381164843
GabrielEvaristoo <GabrielEvaristoo@users.noreply.github.com>
2023-04-18 16:59:30 -0300

    subindo o mod9

---
+++ mod9/testando.java
@@ -0,0 +1,13 @@
+package br.com.evaristo;
+
+public class testando {
+    public static void main(String args[]){
+     System.out.println();
+    long cpf=623253370;
+
+    int cpf2=(int) cpf;
+        System.out.println(cpf2);
+    }
+}
+
+
