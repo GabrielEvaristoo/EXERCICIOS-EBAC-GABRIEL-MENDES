166241d03991bd4c4bfb69384877639480e09dc7
GabrielEvaristoo <GabrielEvaristoo@users.noreply.github.com>
2023-04-24 16:18:32 -0300

    atualizando tarefa mod9

--- mod9/testando.java
+++ mod9/testando.java
@@ -3,10 +3,16 @@ package br.com.evaristo;
 public class testando {
     public static void main(String args[]){
      System.out.println();
+     //criando uma variavel do tipo primitivo e convertendo para o tipo primitivo
     long cpf=623253370;
 
     int cpf2=(int) cpf;
         System.out.println(cpf2);
+        //criando uma variavel do tipo primitivo e convertendo para o tipo wrapper
+        private long telefone=9848585083
+        private Long telefone2= telefone
+        // fazendo unboxing
+        double notas = Double.valueOf();
     }
 }
 
